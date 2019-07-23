c550515c72f35befe32c4b97be354ea944a596fc
Mikhail L <mlozovyywd@gmail.com>
2019-07-23 07:31:00 -0700

    Fixed main.py error

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import webapp2  #connecting to the google app engine yaml file
 import jinja2 #connets to the html files
 import os #apple operating system
-from models import MemeInfo
-
-from google.appengine.api import urlfetch
-import json
+# from google.appengine.api import urlfetch
+# import json
 
 
 #jinja2.Environment is a constructor
@@ -13,3 +11,14 @@ jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class WelcomePage(webapp2.RequestHandler):
+    def get(self):
+        welcome_page = jinja_env.get_template('index.html')
+        self.response.write(welcome_page.render())
+
+
+# the app configuration section
+app = webapp2.WSGIApplication(
+    [('/', WelcomePage)], debug = True
+)
